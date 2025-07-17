41ed26fcbaef58004647a241a4e70e98d474a707
ReleaseHaven <victor.marstrand@hotmail.com>
2025-07-17 13:32:44 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '4rem' }}>
+      <h1>🎶 Welcome to ReleaseHaven</h1>
+      <p>Your home for smarter music releases</p>
+    </div>
+  );
+}
